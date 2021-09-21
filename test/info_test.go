cb49efca6fdd3cc8d9ee1b29099f672e7a645687
Ilkut Kutlar <ilkutkutlar@gmail.com>
2021-09-21 23:33:30 +0100

    add some test stubs

---
+++ test/info_test.go
@@ -0,0 +1,23 @@
+package archive
+
+import (
+  "testing"
+)
+
+func TestListFilesInArchive(t *testing.T) {
+  t.Skip()
+}
+
+func TestListFilesInArchiveWithSpacesInName(t *testing.T) {
+  t.Skip()
+}
+
+
+func TestListTopLevelFilesInArchive(t *testing.T) {
+  t.Skip()
+}
+
+
+func TestListTopLevelFilesInArchiveWithSpacesInName(t *testing.T) {
+  t.Skip()
+}
--- test/manage_test.go
+++ test/manage_test.go
@@ -163,6 +163,22 @@ func TestErrorHandledCorrectlyDuringArchivingGzipped(t *testing.T) {
   t.SkipNow()
 }
 
+func TestUnarchiveFilesFromArchive(t *testing.T) {
+  t.SkipNow()
+}
+
+func TestUnarchiveFilesWithSpacesInNameFromArchive(t *testing.T) {
+  t.SkipNow()
+}
+
+func TestUnarchiveFilesFromArchiveAndRemove(t *testing.T) {
+  t.SkipNow()
+}
+
+func TestUnarchiveFilesWithSpacesInNameFromArchiveAndRemove(t *testing.T) {
+  t.SkipNow()
+}
+
 func cleanup() {
   os.Remove(TEST_ARCHIVE)
 }
