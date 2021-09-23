d172e1c63459a51a0f4ffc398da7bc8c7a10c793
Ilkut Kutlar <ilkutkutlar@gmail.com>
2021-09-24 00:30:32 +0100

    add info tests

--- test/info_test.go
+++ test/info_test.go
@@ -2,22 +2,55 @@ package archive
 
 import (
   "testing"
+  archive "example.com/archive/src"
 )
 
 func TestListFilesInArchive(t *testing.T) {
-  t.Skip()
+  actual, _ := archive.ListArchive("fixtures/dummy_archive.tar")
+  expected := `test_dir/
+test_dir/test1.txt
+test_dir/test_subdir/
+test_dir/test_subdir/test2.txt
+test.txt
+`
+
+  if actual != expected {
+    t.Fail()
+  }
 }
 
 func TestListFilesInArchiveWithSpacesInName(t *testing.T) {
-  t.Skip()
+  actual, _ := archive.ListArchive("fixtures/dummy_archive2.tar")
+  expected := `test.txt
+dir with spaces/
+dir with spaces/file with spaces.txt
+`
+
+  if actual != expected {
+    t.Fail()
+  }
 }
 
 
 func TestListTopLevelFilesInArchive(t *testing.T) {
-  t.Skip()
+  actual, _ := archive.ListArchiveTopLevel("fixtures/dummy_archive.tar")
+  expected := `test_dir/
+test.txt
+`
+
+  if actual != expected {
+    t.Fail()
+  }
 }
 
 
 func TestListTopLevelFilesInArchiveWithSpacesInName(t *testing.T) {
-  t.Skip()
+  actual, _ := archive.ListArchiveTopLevel("fixtures/dummy_archive2.tar")
+  expected := `test.txt
+dir with spaces/
+`
+
+  if actual != expected {
+    t.Fail()
+  }
 }
